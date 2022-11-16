4aa9db7e3c4a055731de62e718ec28d363e41097
Lucio Costa <luciosdc@outlook.com>
2022-11-16 00:33:53 -0300

    Update vetorproject.js

--- aulaswebmoderno/array/vetorproject.js
+++ aulaswebmoderno/array/vetorproject.js
@@ -1,11 +1,10 @@
+// let enqueue = new Array ('Bia', 'Carlos', 'Ana')
+// console.log(enqueue)
 
-
-let enqueue = new Array ('Bia', 'Carlos', 'Ana')
-console.log(enqueue)
-
-aprovados = ['Bia', 'Carlos', 'Ana']
-console.log(enqueue   [0]) // todo indice [0] e iniciado como zero
+enqueue = ['Bia', 'Carlos', 'Ana']
+console.log(enqueue   [0]) 
 console.log(enqueue   [1])
 console.log(enqueue   [2])
 console.log(enqueue   [3])
 
+let dequeue
